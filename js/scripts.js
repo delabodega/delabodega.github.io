20e5f00fabdeaa843810dc076171f4975a3d75ae
Daniel Graf <grafmark59@gmail.com>
2018-06-12 20:21:06 -0400

    tweaked animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
   $('a[href*=\\#]').on('click', function(e) {
     e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 1000, 'easeInOutSine');
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top-10}, 1250, 'easeInOutSine');
   });
 });
