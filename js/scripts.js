17a55cbcb8ca59b4d76fca3e5c8aa59fa0f103b3
Daniel Graf <danielgraf.developer@gmail.com>
2019-07-23 14:53:15 -0400

    Initial About Page

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,15 @@ $(function() {
     $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top-10}, 1250, 'easeInOutSine');
   });
 });
+
+function openNav() {
+  console.log('TEST1')
+  var x = document.getElementById("sideBar");
+  if (x.className === "") {
+    console.log('TEST2')
+    x.className = "open";
+  } else {
+    console.log('TEST3')
+    x.className = "";
+  }
+}
